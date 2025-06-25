7d64d7a9d546d883ad2bb56b7726d34364c62e4d
Arshit Tiwari <arshitt@adobe.com>
2025-06-25 16:30:32 +0530

    landing page + global styling

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,10 @@
 export default async function decorate(block){
-    console.log(block);
+    // const element = document.createElement('div');
+    // element.classList.add('fake-hero');
+    // element.append(block);
+    // console.log(element);
+    block.classList.add('container');
+    block.children[0].classList.add('hero-text');
+    // const button = document.querySelector('hero-text');
+    console.log(button);
 }
