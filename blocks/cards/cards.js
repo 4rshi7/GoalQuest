af6f419a62c7a0cccffd6f1cec4c4c78d60a5bff
Arshit Tiwari <arshitt@adobe.com>
2025-07-03 12:20:33 +0530

    added feature cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,32 @@
+/* eslint-disable padded-blocks */
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+// const container = document.querySelector('.cards-container');
+
+// // 
+ 
+// // Create the wrapper div
+// const wrapper = document.createElement('div');
+// wrapper.className = 'wrapper';
+ 
+// // Move the first and second children into the wrapper
+// while (container.firstChild) {
+//   wrapper.appendChild(container.firstChild);
+// }
+// // Append the wrapper to the container
+// container.appendChild(wrapper);
+  // block.classList.add('container');
+  [...block.children].forEach((child) => {
+    // console.log(child);
+    child.classList.add('feature-card');
+    // child.classlist.add('feature-card');
+    // child.firstElementChild.classList.add('icon');
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
+
+  // console.log(block);
+  /* change to ul, li */
+  // ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
 }
