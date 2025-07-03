6083ad417fd70ee4e44c1bb8f99b3baf207da4e7
Arshit Tiwari <arshitt@adobe.com>
2025-07-03 12:21:21 +0530

    designed the footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -18,6 +18,7 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  
 
   block.append(footer);
 }
