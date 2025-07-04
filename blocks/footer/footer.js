fc4e3cd27b534a1b3bd2afc1f02a5cf0da476465
arshit <arshit27202@gmail.com>
2025-07-04 21:04:30 +0530

    carousel changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -10,7 +10,7 @@ export default async function decorate(block) {
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta
     ? new URL(footerMeta, window.location).pathname
-    : '/footer';footerPathfooterMetanewURL.pathname
+    : '/footer';
   // footerPathfooterMetanewURL.pathname;
   const fragment = await loadFragment(footerPath);
 
