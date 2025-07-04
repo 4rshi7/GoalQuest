5b5c82810d98cd65fd83e27050e63e42e4e0a6a5
arshit <arshit27202@gmail.com>
2025-07-04 21:10:38 +0530

    linting fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,11 +12,7 @@ module.exports = {
   },
   rules: {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-<<<<<<< HEAD
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks         revert before commit to unix
-=======
-    'linebreak-style': ['error', 'windows'], // enforce unix linebreaks         revert before commit to unix
->>>>>>> a333d43 (minor cosmetic changes)
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
   },
 };
